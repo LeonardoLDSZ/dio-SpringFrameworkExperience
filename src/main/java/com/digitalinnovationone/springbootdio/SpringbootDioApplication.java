13c539b81ed7e5c4ab76c20f337a2a71e216913d
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-08-19 19:26:41 -0300

    First commit

---
+++ src/main/java/com/digitalinnovationone/springbootdio/SpringbootDioApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.springbootdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDioApplication.class, args);
+	}
+
+}
