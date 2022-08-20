1c93fb6bca1c475eddcf2088afba7c03b0cb42ed
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-08-20 14:26:40 -0300

    First commit

---
+++ src/main/java/com/firstproject/dio/diospringbootwebdio/DioSpringbootWebDioApplication.java
@@ -0,0 +1,13 @@
+package com.firstproject.dio.diospringbootwebdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringbootWebDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringbootWebDioApplication.class, args);
+	}
+
+}
