b45221b2a3a91107d6ef8bcd69000416b07a4554
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-08-20 14:04:10 -0300

    first commit

---
+++ src/main/java/digitalInnovationOne/diospringbootdependencyinjection/DioSpringbootDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package digitalInnovationOne.diospringbootdependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringbootDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringbootDependencyInjectionApplication.class, args);
+	}
+
+}
