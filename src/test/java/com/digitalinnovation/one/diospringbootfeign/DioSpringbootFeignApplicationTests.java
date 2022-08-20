83e78b4db79e1206871acba54baa8b83d705841f
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-08-20 15:07:39 -0300

    first commit

---
+++ src/test/java/com/digitalinnovation/one/diospringbootfeign/DioSpringbootFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.one.diospringbootfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringbootFeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
