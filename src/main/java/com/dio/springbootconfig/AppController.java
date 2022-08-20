ec59349a9940ea806e5346e0e00000b7567ac445
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-08-20 08:51:06 -0300

    ending class

--- src/main/java/com/dio/springbootconfig/AppController.java
+++ src/main/java/com/dio/springbootconfig/AppController.java
@@ -10,10 +10,15 @@ public class AppController {
     @Value("${app.message}")
     private String appMessage;
 
+    @Value("${ENV_DB_URL:NONE}")
+    private String dbEnvironmentVariable;
     @GetMapping("/")
     public String getAppMessage(){
         return appMessage;
 
     }
-
+    @GetMapping("/envVariable")
+    public String getDbEnvironmentVariable() {
+        return "The following environment variable was passed: " + dbEnvironmentVariable;
+    }
 }
