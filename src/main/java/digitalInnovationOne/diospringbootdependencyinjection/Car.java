1debd090223002c57ae684c5e0e9eef296a04550
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-08-20 14:09:41 -0300

    Class completed

---
+++ src/main/java/digitalInnovationOne/diospringbootdependencyinjection/Car.java
@@ -0,0 +1,9 @@
+package digitalInnovationOne.diospringbootdependencyinjection;
+
+public class Car implements Vehicle {
+
+    @Override
+    public void action(){
+        System.out.println("It's a car");
+    }
+}
