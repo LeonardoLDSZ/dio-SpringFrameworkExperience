2624be665f2c869fec0354efddd944e92cb98dbb
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-08-20 15:55:02 -0300

    first commit

---
+++ src/main/java/com/digitalinnovation/one/diounittests/DioUnittestsApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.one.diounittests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioUnittestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioUnittestsApplication.class, args);
+	}
+
+}
