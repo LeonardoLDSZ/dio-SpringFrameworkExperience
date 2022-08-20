ba6e179f014227a3f89947b0e4fd3db195b43dee
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-08-20 14:41:59 -0300

    First commit

---
+++ src/main/java/com/digitalinnovation/one/diospringbootswagger/DioSpringbootSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.one.diospringbootswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringbootSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringbootSwaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalinnovation/one/diospringbootswagger/DioSpringbootSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.one.diospringbootswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringbootSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
