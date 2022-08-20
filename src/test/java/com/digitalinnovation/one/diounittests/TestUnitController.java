9199bf2965e5ca10075b3a5c140cc1a68528f1f3
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-08-20 16:57:13 -0300

    testing unit

---
+++ src/test/java/com/digitalinnovation/one/diounittests/TestUnitController.java
@@ -0,0 +1,16 @@
+package com.digitalinnovation.one.diounittests;
+
+
+import controller.TestController;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestUnitController {
+
+    @Test
+    public void testUnit() {
+        TestController controller = new TestController();
+        String resultado = controller.saudacao("DIO");
+        assertEquals("Bem-vindo, DIO", resultado);
+    }
+}
