8ea794c186098373c5e09871d06a476daa46a076
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-08-21 15:23:25 -0300

    commit - Patterns Java

---
+++ src/main/java/digitalinnovationone/diolabpartternsjav/DioLabPartternsJavApplication.java
@@ -0,0 +1,13 @@
+package digitalinnovationone.diolabpartternsjav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioLabPartternsJavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioLabPartternsJavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/digitalinnovationone/diolabpartternsjav/DioLabPartternsJavApplicationTests.java
@@ -0,0 +1,13 @@
+package digitalinnovationone.diolabpartternsjav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioLabPartternsJavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
